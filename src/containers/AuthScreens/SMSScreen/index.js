7fec71073ffc274646e579386a3c53fe49283063
Vladimir <vladimir.dev.1013@gmail.com>
2019-03-08 01:59:42 +0700

    implemented template screens

---
+++ src/containers/AuthScreens/SMSScreen/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { ImageBackground, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { styles } from './styles';
+
+class SMSScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <ImageBackground/>
+    )
+  }
+}
+
+export default SMSScreen;
---
+++ src/containers/index.js
@@ -0,0 +1,15 @@
+import SelectScreen from './AuthScreens/SelectScreen';
+import ConfirmSMSScreen from './AuthScreens/ConfirmSMSScreen';
+import InitialScreen from './AuthScreens/InitialScreen';
+import SignInScreen from './AuthScreens/SignInScreen';
+import SignUpScreen from './AuthScreens/SignUpScreen';
+import SMSScreen from './AuthScreens/SMSScreen';
+
+export {
+  SelectScreen,
+  ConfirmSMSScreen,
+  InitialScreen,
+  SignUpScreen,
+  SignInScreen,
+  SMSScreen
+};
