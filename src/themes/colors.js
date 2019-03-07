0df3864b4cd912878b8c8c7227193def8d2024a1
Vladimir <vladimir.dev.1013@gmail.com>
2019-03-08 01:53:24 +0700

    implemented themes

---
+++ src/themes/colors.js
@@ -0,0 +1,14 @@
+export default {
+  background: '#272839',
+  white: 'white',
+  red: '#f40323',
+  red1: '#f80322',
+  lightGrey: '#a4a9ad',
+  grey: '#6c6f79',
+  dark: '#3d3e4d',
+  border: '#84858f',
+  gray: '#474857',
+  green: '#46d400',
+  darkGreen: '#2b4331',
+  darkBlack: '#1e1d25',
+}
