b183771f9d816ab3690aee9a4c5e2f01634b5bc2
Vladimir <vladimir.dev.1013@gmail.com>
2019-03-08 16:33:49 +0700

    implemented UI of SMSScreen

--- src/containers/AuthScreens/SMSScreen/index.js
+++ src/containers/AuthScreens/SMSScreen/index.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
-import { ImageBackground, View } from 'react-native';
+import { ImageBackground, View, Text, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import { styles } from './styles';
+import {CloseButton, RedButton} from "../../../components/Buttons";
 
 class SMSScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      number: ''
     }
   }
 
+  onClose = () => {
+    alert('close');
+  };
+
+  onContinue = () => {
+    alert('continue');
+  };
+
+  onChangeText = (number) => {
+    this.setState({ number });
+  };
+
   render() {
+    const { number } = this.state;
     return (
-      <ImageBackground/>
-    )
+      <View style={styles.container}>
+        <SafeAreaView style={styles.parentContainer}>
+          <ImageBackground
+            source={require('../../../assets/img_background4.png')}
+            style={styles.container}
+            imageStyle={styles.backgroundImg}
+          >
+            <View style={styles.closeContainer}>
+              <CloseButton onPress={this.onClose} />
+            </View>
+            <Text style={styles.titleText}>
+              {`Confirm using\nyour Phone Number`}
+            </Text>
+            <View style={styles.mainContainer}>
+              <View style={styles.inputContainer}>
+                <View style={styles.countryCodeContainer}>
+                  <Text style={styles.titleText}>
+                    +1
+                  </Text>
+                </View>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={this.onChangeText}
+                  value={number}
+                  keyboardType="numeric"
+                />
+              </View>
+              <Text style={styles.whiteText}>
+                We will send you One-Time code.
+              </Text>
+              <RedButton label="Continue" onPress={this.onContinue} style={styles.btnContainer}/>
+            </View>
+          </ImageBackground>
+          <View style={styles.bottomContainer} />
+        </SafeAreaView>
+      </View>
+    );
   }
 }
 
--- src/containers/AuthScreens/SMSScreen/styles.js
+++ src/containers/AuthScreens/SMSScreen/styles.js
@@ -1,8 +1,69 @@
 import { StyleSheet } from 'react-native';
+import { Colors, Metric } from '../../../themes';
 
 export const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    backgroundColor: '#131947'
+  },
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  backgroundImg: {
+    height: 160,
+    resizeMode: 'stretch'
+  },
+  mainContainer: {
+    flex: 1,
+    marginTop: 80,
+    alignItems: 'center'
+  },
+  titleText: {
+    ...Metric.font.big,
+    color: Colors.white,
+    textAlign: 'center',
+  },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 50,
+    backgroundColor: Colors.background
+  },
+  closeContainer: {
+    alignItems: 'flex-end',
+    marginRight: Metric.marginHorizontal
+  },
+  whiteText: {
+    ...Metric.font.h4,
+    color: Colors.white,
+    textAlign: 'center'
+  },
+  btnContainer: {
+    marginTop: Metric.height / 10
+  },
+  inputContainer: {
+    height: 45,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.border,
+    flexDirection: 'row',
+    paddingVertical: 5,
+    marginBottom: Metric.height / 30
+  },
+  countryCodeContainer: {
+    marginLeft: 5,
+    paddingRight: 10,
+    borderRightWidth: 1,
+    borderRightColor: Colors.border,
+    marginBottom: 5
+  },
+  input: {
+    marginLeft: 10,
+    width: Metric.width / 5 * 2.5,
+    ...Metric.font.big,
+    color: Colors.white
   }
 });
 
